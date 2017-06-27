2db853eea133fa34c7852bc9e40995a598a950be
Gabriel Becker <gabrieloliveirabecker@gmail.com>
2017-06-27 18:36:48 -0300

    Add class the Task.h for definition to tasks

---
+++ src/inc/task.h
@@ -0,0 +1,16 @@
+#ifndef     SRC_INC_TASK_H
+#define     SRC_INC_TASK_H
+
+#include    <iostream>
+
+class Task
+{
+public:
+    virtual void execute () = 0;
+
+private:
+
+};
+
+#endif      //endif SRC_INC_TASK_H
+
